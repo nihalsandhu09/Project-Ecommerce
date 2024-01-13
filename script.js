4f78575afaa244393ba79ae5ac038d029d2de11a
Nihal SANDHU <nihalsandhu007220@gmail.com>
2024-01-13 09:47:32 +0530

    slider done

---
+++ script.js
@@ -0,0 +1,12 @@
+const wrapper = document.querySelector(".sliderWrapper") 
+
+const menuItem = document.querySelectorAll(".menuItem")
+
+menuItem.forEach((item,index)=>{
+  console.log(item , index)
+    item.addEventListener("click",function(event){
+    wrapper.style.transform = `translateX(${-100 *index}vw)`
+    })
+     
+
+})

d1a60416b72d1b89c2930b482d4db91b2f643a80
Nihal SANDHU <nihalsandhu007220@gmail.com>
2024-01-15 19:30:56 +0530

    features  section

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ const menuItem = document.querySelectorAll(".menuItem")
 menuItem.forEach((item,index)=>{
   console.log(item , index)
     item.addEventListener("click",function(event){
-    wrapper.style.transform = `translateX(${-100 *index}vw)`
+
+    wrapper.style.transform = `translateX(${-100 * index}vw)`
+
     })
      
 

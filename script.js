2972f1e9f6b8c4a64626428b945358e152222465
Nihal SANDHU <nihalsandhu007220@gmail.com>
2024-02-01 18:05:33 +0530

    just addign Product section functiuonality

--- script.js
+++ script.js
@@ -2,13 +2,98 @@ const wrapper = document.querySelector(".sliderWrapper")
 
 const menuItem = document.querySelectorAll(".menuItem")
 
+const products = [
+  {
+    id: 1,
+    title: "Air Force",
+    price: 119,
+    colors: [
+      {
+        code: "black",
+        img: "./img/air.png",
+      },
+      {
+        code: "darkblue",
+        img: "./img/air2.png",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Air Jordan",
+    price: 149,
+    colors: [
+      {
+        code: "lightgray",
+        img: "./img/jordan.png",
+      },
+      {
+        code: "green",
+        img: "./img/jordan2.png",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Blazer",
+    price: 109,
+    colors: [
+      {
+        code: "lightgray",
+        img: "./img/blazer.png",
+      },
+      {
+        code: "green",
+        img: "./img/blazer2.png",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Crater",
+    price: 129,
+    colors: [
+      {
+        code: "black",
+        img: "./img/crater.png",
+      },
+      {
+        code: "lightgray",
+        img: "./img/crater2.png",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Hippie",
+    price: 99,
+    colors: [
+      {
+        code: "gray",
+        img: "./img/hippie.png",
+      },
+      {
+        code: "black",
+        img: "./img/hippie2.png",
+      },
+    ],
+  },
+];
+ let chooseProduct = products[0]
+ const currentProduct = document.querySelector(".productImg")
+ const currentProductTitle = document.querySelector(".productTitle")
+ const currentProductPrice = document.querySelector(".productPrice");
+const currentProductColors = document.querySelectorAll(".color");
+const currentProductSizes = document.querySelectorAll(".size");
 menuItem.forEach((item,index)=>{
   console.log(item , index)
     item.addEventListener("click",function(event){
-
+  //  change the current slide
     wrapper.style.transform = `translateX(${-100 * index}vw)`
-
+  //  change the choosen Product
+  chooseProduct= products[index]
     })
      
 
 })
+

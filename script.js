7c2c101b4fd734b4e5e034f757f8c685c468b0a1
Nihal SANDHU <nihalsandhu007220@gmail.com>
2024-02-04 00:04:07 +0530

    just commit

--- script.js
+++ script.js
@@ -79,8 +79,8 @@ const products = [
     ],
   },
 ];
- let chooseProduct = products[0]
- const currentProduct = document.querySelector(".productImg")
+ let choosenProduct = products[0]
+ const currentProductImg = document.querySelector(".productImg")
  const currentProductTitle = document.querySelector(".productTitle")
  const currentProductPrice = document.querySelector(".productPrice");
 const currentProductColors = document.querySelectorAll(".color");
@@ -91,9 +91,47 @@ menuItem.forEach((item,index)=>{
   //  change the current slide
     wrapper.style.transform = `translateX(${-100 * index}vw)`
   //  change the choosen Product
-  chooseProduct= products[index]
+  choosenProduct= products[index]
+  
+    //change texts of currentProduct
+    currentProductTitle.textContent = choosenProduct.title;
+    currentProductPrice.textContent = "$" + choosenProduct.price;
+    currentProductImg.src = choosenProduct.colors[0].img;
+
+    //assing new colors
+    currentProductColors.forEach((color, index) => {
+      color.style.backgroundColor = choosenProduct.colors[index].code;
+    });
     })
      
 
 })
 
+currentProductColors.forEach((color, index) => {
+  color.addEventListener("click", () => {
+    currentProductImg.src = choosenProduct.colors[index].img;
+  });
+});
+
+currentProductSizes.forEach((size, index) => {
+  size.addEventListener("click", () => {
+    currentProductSizes.forEach((size) => {
+      size.style.backgroundColor = "white";
+      size.style.color = "black";
+    });
+    size.style.backgroundColor = "black";
+    size.style.color = "white";
+  });
+});
+
+const productButton = document.querySelector(".productButton");
+const payment = document.querySelector(".payment");
+const close = document.querySelector(".close");
+
+productButton.addEventListener("click", () => {
+  payment.style.display = "flex";
+});
+
+close.addEventListener("click", () => {
+  payment.style.display = "none";
+});
